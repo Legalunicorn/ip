1547238159b74954366b7d1314a2ebc1f9fd5281
Nathan Yeo w11 <nyeo700@gmail.com>
2026-08-21 21:32:58 +0800

    Expand EventTest coverage

--- src/test/java/bingus/task/EventTest.java
+++ src/test/java/bingus/task/EventTest.java
@@ -27,4 +27,25 @@ public class EventTest {
                 LocalDateTime.of(2026, 8, 22, 18 ,0));
         assertFalse(e.matchesDate(LocalDate.of(2026,7,21)));
     }
+
+    @Test
+    void matchesDate_startAndEndDate_returnsTrue() {
+        Event e = new Event(
+                "camp",
+                LocalDateTime.of(2026, 8, 20, 9, 0),
+                LocalDateTime.of(2026, 8, 22, 18, 0));
+
+        assertTrue(e.matchesDate(LocalDate.of(2026, 8, 20)));
+        assertTrue(e.matchesDate(LocalDate.of(2026, 8, 22)));
+    }
+
+    @Test
+    void matchesDate_dateAfterEvent_returnsFalse() {
+        Event e = new Event(
+                "camp",
+                LocalDateTime.of(2026, 8, 20, 9, 0),
+                LocalDateTime.of(2026, 8, 22, 18, 0));
+
+        assertFalse(e.matchesDate(LocalDate.of(2026, 8, 23)));
+    }
 }
