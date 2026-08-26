41af56676d17f153745cd270bf60cfa9d89cca46
Nathan Yeo w11 <nyeo700@gmail.com>
2026-08-27 00:20:09 +0800

    Set GUI window size limits

--- src/main/java/bingus/Main.java
+++ src/main/java/bingus/Main.java
@@ -26,6 +26,8 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setMinHeight(367);
+            stage.setMinWidth(467);
             fxmlLoader.<MainWindow>getController().setDuke(bingus);
             stage.show();
         } catch (IOException e) {
